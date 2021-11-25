73e23b11e4f73a930560229d27229ed312a6d137
saraswatikumari <saraswati.kumari_cs19@gla.ac.in>
2021-11-25 19:55:59 +0530

    Initial commit

---
+++ src/B.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+    public class B {
+        public static void main(String[] args) {
+            {
+                int a[]=new  int[5];
+                int a1[]=new int[8];
+                Scanner r=new Scanner(System.in);
+                boolean b= Arrays.equals(a,a1);
+                System.out.println("array are equal "+b);
+
+
+            }
+        }
+
+
+
+
+    }
+
+
+}
